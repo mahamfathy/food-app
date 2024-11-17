b584de715fe97ccba4840d6b02188a462d9f8503
mahamfathy <mahafathyh@gmail.com>
2024-11-17 17:00:22 +0200

    finish togglepassword visibility in register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  hidePassword: boolean = true;
   constructor() {}
   registerForm: FormData = new FormData();
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
   onRegister(): void {}
 }
