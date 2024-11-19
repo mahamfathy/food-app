e46d51582501fcc6269b7dc029d0208f6fbdc5ee
mahamfathy <mahafathyh@gmail.com>
2024-11-19 18:13:29 +0200

    finish rsetpassword styling and ts

--- src/app/auth/components/reset-passowrd/reset-passowrd.component.ts
+++ src/app/auth/components/reset-passowrd/reset-passowrd.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-reset-passowrd',
   templateUrl: './reset-passowrd.component.html',
-  styleUrls: ['./reset-passowrd.component.scss']
+  styleUrls: ['./reset-passowrd.component.scss'],
 })
 export class ResetPassowrdComponent {
-
+  resetPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    seed: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){2,})(?=(.*[!@#$%^&*()-__+.]){1,}).{8,}$'
+      ),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){2,})(?=(.*[!@#$%^&*()-__+.]){1,}).{8,}$'
+      ),
+    ]),
+  });
+  constructor(private authService: AuthService) {}
+  onResetPass(resetPassForm: FormGroup): void {
+    this.authService.resetPasswordForm(resetPassForm.value).subscribe();
+    resetPassForm.reset();
+  }
 }
