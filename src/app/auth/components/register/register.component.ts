27297ff1ee6e840f3eafefa7b37a05e4a2e55221
mahamfathy <mahafathyh@gmail.com>
2024-11-19 22:11:01 +0200

    setItem username in register and getItem it in home component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../service/auth.service';
+import { LocalStorageService } from '../../service/local-storage.service';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +17,10 @@ export class RegisterComponent {
   resMessage: string = '';
   constructor(
     private _FormBuilder: FormBuilder,
-    private _AuthService: AuthService
+    private _AuthService: AuthService,
+    private _ToastrService: ToastrService,
+    private _LocalStorageService: LocalStorageService,
+    private _Router: Router
   ) {
     this.registerForm = this._FormBuilder.group({
       userName: ['', Validators.required],
@@ -59,10 +65,16 @@ export class RegisterComponent {
     }
     this._AuthService.registerForm(this.registerForm.value).subscribe({
       next: (res) => {
-        console.log(res);
+        this.resMessage = res.message;
       },
       error: (err) => {
-        console.log(err);
+        this._ToastrService.error(err.error.message, 'Error');
+      },
+      complete: () => {
+        const userName = this.registerForm.value.userName;
+        this._ToastrService.success(this.resMessage, 'Success');
+        this._LocalStorageService.setItem('userName', userName);
+        this._Router.navigateByUrl('/dashboard/home');
       },
     });
   }
--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/auth/service/local-storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  userName: string = '';
+  constructor(private _LocalStorageService: LocalStorageService) {}
+  ngOnInit(): void {
+    this.userName = this._LocalStorageService.getItem('userName') || '';
+  }
 }
