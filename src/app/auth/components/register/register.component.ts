1e8c22039dad09e86b6e12463d35e69f4e09ee63
mahamfathy <mahafathyh@gmail.com>
2024-11-19 21:59:16 +0200

    create method on register in regigseter component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IRegister } from '../../interface/IRegister';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,37 +10,60 @@ import { IRegister } from '../../interface/IRegister';
 export class RegisterComponent {
   hidePassword: boolean = true;
   files: File[] = [];
-  registerForm: FormData = new FormData();
-  registerData: IRegister = {
-    username: '',
-    email: '',
-    phone: '',
-    country: '',
-    password: '',
-    confirmPassword: '',
-  };
-  constructor() {}
+  registerForm!: FormGroup;
+  resMessage: string = '';
+  constructor(
+    private _FormBuilder: FormBuilder,
+    private _AuthService: AuthService
+  ) {
+    this.registerForm = this._FormBuilder.group({
+      userName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      country: ['', Validators.required],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){2,})(?=(.*[!@#$%^&*()-__+.]){1,}).{8,}$'
+          ),
+        ],
+      ],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
   onSelect(event: { addedFiles: File[] }) {
-    console.log(event);
     this.files.push(...event.addedFiles);
   }
 
   onRemove(event: File) {
-    console.log(event);
     this.files.splice(this.files.indexOf(event), 1);
   }
   togglePasswordVisibility(): void {
     this.hidePassword = !this.hidePassword;
   }
   onRegister(): void {
-    // this.registerForm.append('username', this.registerData.username);
-    // this.registerForm.append('email', this.registerData.email);
-    // this.registerForm.append('phone', this.registerData.phone);
-    // this.registerForm.append('country', this.registerData.country);
-    // this.registerForm.append('password', this.registerData.password);
-    // this.registerForm.append(
-    //   'confirmPassword',
-    //   this.registerData.confirmPassword
-    // );
+    if (this.registerForm.valid) {
+      const formData = new FormData();
+      Object.keys(this.registerForm.value).forEach((key) => {
+        formData.append(key, this.registerForm.value[key]);
+      });
+      this.files.forEach((file, index) => {
+        formData.append(`file_${index}`, file, file.name);
+      });
+      formData.forEach((value, key) => {
+        console.log(key, value);
+      });
+    }
+    this._AuthService.registerForm(this.registerForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
