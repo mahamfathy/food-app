9722f89d20d2df71ccd40c68628d2681420569a4
mahamfathy <mahafathyh@gmail.com>
2024-11-21 19:15:24 +0200

    finish user guard

--- src/app/core/Guards/user.guard.ts
+++ src/app/core/Guards/user.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/auth/service/local-storage.service';
 
 export const userGuard: CanActivateFn = (route, state) => {
-  return true;
+  const _LocalStorageService = inject(LocalStorageService);
+  const _Router = inject(Router);
+
+  const role = _LocalStorageService.getItem('role');
+  if (role === 'SystemUser') {
+    return true;
+  } else {
+    _Router.navigate(['/dashboard']);
+    return false;
+  }
 };
