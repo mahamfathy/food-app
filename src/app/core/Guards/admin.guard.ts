2256d6c3b1bafd7e946e2c10d3d061948b83f928
mahamfathy <mahafathyh@gmail.com>
2024-11-21 19:14:21 +0200

    finish admin guard

--- src/app/core/Guards/admin.guard.ts
+++ src/app/core/Guards/admin.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/auth/service/local-storage.service';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+  const _LocalStorageService = inject(LocalStorageService);
+  const _Router = inject(Router);
+  const role = _LocalStorageService.getItem('role');
+  if (role === 'SuperAdmin') {
+    return true;
+  } else {
+    _Router.navigate(['/dashboard']);
+    return false;
+  }
 };
