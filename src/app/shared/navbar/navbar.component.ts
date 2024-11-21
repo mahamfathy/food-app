7ed32a7bd6781b91697f6b455b02ecc01e6577a3
mahamfathy <mahafathyh@gmail.com>
2024-11-22 00:05:38 +0200

    finish styling nabvar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from 'src/app/auth/service/local-storage.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  userName: string | null = '';
+  constructor(private _LocalStorageService: LocalStorageService) {
+    this.userName = _LocalStorageService.getItem('userName');
+  }
 }
