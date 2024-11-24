746a17904a426df4ff84ee8f2eac5fb8ffa18aea
mahamfathy <mahafathyh@gmail.com>
2024-11-24 05:12:49 +0200

    create category service

---
+++ src/app/admin/services/category.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  constructor(private _HttpClient: HttpClient) {}
+  getCategory(): Observable<any> {
+    return this._HttpClient.get('/Category');
+  }
+}
