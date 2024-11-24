25789ff630b65ca04c0e6c494cc2c3c1044dd320
mahamfathy <mahafathyh@gmail.com>
2024-11-24 06:09:02 +0200

    finish get categories styling aand methods

--- src/app/admin/categories/categories.component.ts
+++ src/app/admin/categories/categories.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent {
-
+export class CategoriesComponent implements OnInit {
+  categoryName: string[] = [];
+  constructor(private _CategoryService: CategoryService) {}
+  ngOnInit(): void {
+    this._CategoryService.getCategories().subscribe({
+      next: (res) => {
+        res.data.forEach((category: any) => {
+          this.categoryName.push(category.name);
+        });
+      },
+    });
+  }
 }
--- src/app/admin/services/category.service.ts
+++ src/app/admin/services/category.service.ts
@@ -1,13 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LocalStorageService } from 'src/app/auth/service/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
-  constructor(private _HttpClient: HttpClient) {}
-  getCategory(): Observable<any> {
-    return this._HttpClient.get('/Category');
+  headers: any = new HttpHeaders({
+    Authorization: `Bearer ${this._LocalStorageService.getItem('userToken')}`,
+  });
+  constructor(
+    private _HttpClient: HttpClient,
+    private _LocalStorageService: LocalStorageService
+  ) {}
+  getCategories(): Observable<any> {
+    return this._HttpClient.get('/Category', {
+      headers: this.headers,
+    });
   }
 }
