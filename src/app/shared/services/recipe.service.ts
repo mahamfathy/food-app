d8c1344147209d12e2c3268397098968a3494434
mahamfathy <mahafathyh@gmail.com>
2024-11-25 02:43:53 +0200

    create recipe service

---
+++ src/app/shared/services/recipe.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from 'src/app/auth/service/local-storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  headers: any = new HttpHeaders({
+    Authorization: `Bearer ${this._LocalStorageService.getItem('userToken')}`,
+  });
+  constructor(
+    private _HttpClient: HttpClient,
+    private _LocalStorageService: LocalStorageService
+  ) {}
+  // getRecipes(): Observable<any> {
+  // }
+  // addRecipe(Recipe: string): Observable<any> {
+  // }
+  getRecipeById(id: number): Observable<any> {
+    return this._HttpClient.get(`/Recipe/${id}`);
+  }
+  // updateRecipe(): Observable<any> {
+
+  // }
+  deleteRecipe(id: number): Observable<any> {
+    return this._HttpClient.delete(`/Recipe/${id}`);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent, HomeComponent],
+  declarations: [NavbarComponent, SidebarComponent, HomeComponent, RecipesComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, SidebarComponent],
 })
