ed6c9fea8d8d35a68075f62f2856af24bdc35933
mahamfathy <mahafathyh@gmail.com>
2024-11-26 23:33:33 +0200

    create dropdown from angular material

--- src/app/admin/categories/categories.module.ts
+++ src/app/admin/categories/categories.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from 'src/app/shared/shared.module';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './categories.component';
 
-
 @NgModule({
-  declarations: [
-    CategoriesComponent
-  ],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ]
+  declarations: [CategoriesComponent],
+  imports: [CommonModule, CategoriesRoutingModule, SharedModule],
 })
-export class CategoriesModule { }
+export class CategoriesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent, HomeComponent, RecipesComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SidebarComponent],
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    RecipesComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
+  exports: [
+    NavbarComponent,
+    SidebarComponent,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
 })
 export class SharedModule {}
