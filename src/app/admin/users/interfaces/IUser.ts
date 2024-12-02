08fa263dc19004a7ac8ae6a5f2c6a83cf49859d4
mahamfathy <mahafathyh@gmail.com>
2024-12-02 11:22:17 +0200

    create interface for users

---
+++ src/app/admin/users/interfaces/IUser.ts
@@ -0,0 +1,16 @@
+export interface IUser {
+  id: number;
+  userName: string;
+  email: string;
+  country: string;
+  phoneNumber: number;
+  imagePath: string;
+  creationDate: string;
+  modificationDate: string;
+}
+//    group : {
+//      id : 2,
+//      name :  SystemUser ,
+//      creationDate :  2024-02-10T11:15:00.393Z ,
+//      modificationDate :  2024-02-10T11:15:00.393Z
+//   },
