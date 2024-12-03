bb60786c796f2b12c4b20e144f96708a39c05071
mahamfathy <mahafathyh@gmail.com>
2024-12-03 20:22:00 +0200

    create component of create new admin

--- src/app/admin/users/users.module.ts
+++ src/app/admin/users/users.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 import { ViewUserComponent } from './components/view-user/view-user.component';
+import { CreateAdminComponent } from './components/create-admin/create-admin.component';
 
 @NgModule({
-  declarations: [UsersComponent, ViewUserComponent],
+  declarations: [UsersComponent, ViewUserComponent, CreateAdminComponent],
   imports: [CommonModule, UsersRoutingModule, SharedModule],
 })
 export class UsersModule {}
