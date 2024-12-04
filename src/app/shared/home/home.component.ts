19f1fd408410c26e9a15791bde08469ac2627ff3
mahamfathy <mahafathyh@gmail.com>
2024-12-04 04:53:53 +0200

    finish remove fav recipe method

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LocalStorageService } from 'src/app/auth/service/local-storage.service';
+import { UserService } from 'src/app/admin/users/services/user.service';
+import { AuthService } from 'src/app/auth/service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,16 @@ import { LocalStorageService } from 'src/app/auth/service/local-storage.service'
 })
 export class HomeComponent implements OnInit {
   userName: string = '';
-  constructor(private _LocalStorageService: LocalStorageService) {}
+  constructor(
+    private _UserService: UserService,
+    private _AuthService: AuthService
+  ) {}
   ngOnInit(): void {
-    this.userName = this._LocalStorageService.getItem('userName') || '';
+    this._UserService.userName$.subscribe((name) => {
+      this.userName = name || '';
+    });
+  }
+  isAdmin(): boolean {
+    return this._AuthService.getRole() === 'SuperAdmin';
   }
 }
